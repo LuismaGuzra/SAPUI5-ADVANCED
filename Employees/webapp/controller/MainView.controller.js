9f8433057eaaaaae8aca22045bad583779779b2d
LuismaGuzra <luisragus11@gmail.com>
2022-01-04 18:41:37 +0000

    First commit 04_elemento_contrl_ui5

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 sap.ui.define([
 		"sap/ui/core/mvc/Controller"
 	],
@@ -7,9 +8,25 @@ sap.ui.define([
 	function (Controller) {
 		"use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+		var Main =  Controller.extend("logaligroup.Employees.controller.MainView", {});
 
-			}
-		});
+
+        Main.prototype.onValidate = function () {
+                    var inputEmployee = this.byId("inputEmployee");
+                    var valueEmployee = inputEmployee.getValue();
+
+                    if (valueEmployee.length === 6) {
+                        // inputEmployee.setDescription("OK");
+                        this.getView().byId("labelCountry").setVisible(true);
+                        this.getView().byId("slCountry").setVisible(true);
+                    } else {
+                        // inputEmployee.setDescription("not OK");
+                        this.getView().byId("labelCountry").setVisible(false);
+                        this.getView().byId("slCountry").setVisible(false);
+                    }
+
+                };
+
+           return Main;     
+			
 	});
